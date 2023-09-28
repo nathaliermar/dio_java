aa3a2e9151f4fa4c5065fd7ec67a5141cf034486
nathaliermar <nathalie.rmartines@gmail.com>
2023-09-27 21:25:34 -0300

    variable types examples

---
+++ src/VariableTypes.java
@@ -0,0 +1,21 @@
+public class VariableTypes {
+    public static void main(String [] args) {
+
+        String myName = "Nathalie Ribeiro";
+        double minimumWage = 2200;
+
+        short shortNumber = 2;
+        int normalNumber = shortNumber;
+        short shortNumber2 = (short) normalNumber;
+
+        int number = 4;
+
+        number = 12;
+
+        System.out.println(myName);
+        System.out.println(number);
+
+        final double pi_value = 3.14;
+
+    }
+}
